ccddfb4df3068b56295d135e81de8a8d7fa5e1fc
jashim <jashim2kn3@gmail.com>
2023-11-08 21:23:44 +0530

    added models

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv')
+const Product = require('./models/product')
+const app = express()
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URI)
+app.use(express.json())
+
+app.get('/', (req, res) =>{
+    res.send("Success")
+})
+
+app.get('/products', (req, res) => {
+    console.log("database connected")
+})
+
+app.post('/products', (req, res) =>{
+    const {category, name, description, price, stock} = req.body;
+
+    const products = new Product({category: category, name: name, description: description, price: price, stock: stock});
+
+    products.save()
+    res.send(products)
+})
+
+
+app.listen(3000, console.log("DB conncected and server running"))
--- backend/models/product.js
+++ backend/models/product.js
@@ -1,16 +1,16 @@
 const mongoose = require('mongoose');
-const schema = mongoose.Schema;
 
-const productSchema = new schema(
+
+const productSchema = mongoose.Schema(
     {
-        category: String,
-        name: String,
-        description: String,
-        price: String,
-        stock: Number
+        category: {type: String, required: true},
+        name: {type: String, required: true},
+        description: {type: String, required: true},
+        price: {type: Number, required: true},
+        stock: {type: Number, required: true}
     }
 )
 
-const Product = mongoose.model('Product', productSchema);
 
-module.exports = {Product};
+
+module.exports = mongoose.model('Product', productSchema);
